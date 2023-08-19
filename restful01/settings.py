8ca1dd4a55b99dda8eeb893451557cd34c110f7c
Lera06 <lera06@rambler.ru>
2023-08-19 15:34:13 +0300

    Commit Added Urls and Views to drone app

--- restful01/settings.py
+++ restful01/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +45,7 @@ INSTALLED_APPS = [
 
     # Local
     'toys',
+    'drones',
 
     # 3rd party
     'rest_framework',
@@ -80,13 +86,24 @@ WSGI_APPLICATION = 'restful01.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Postgres:
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'drones',
+        'USER': 'pguser',
+        'PASSWORD': 'Pa$$w0rd',
+        'HOST': 'vpngw.avalon.ru',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
